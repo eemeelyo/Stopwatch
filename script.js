71257a2d199e0978fefcdf2105b54aa38ce445b7
eemeelyo <ecomiling@rocketmail.com>
2024-02-20 03:46:35 +0000

    Updated the functionality of the buttons and added animation on Pause button.

--- script.js
+++ script.js
@@ -22,6 +22,9 @@ resetButton.addEventListener('click', resetTimer);
 function startTimer() {
     interval = setInterval(updateTimer, 10);
     startButton.disabled = true;
+    pauseButton.disabled = false;
+    resetButton.disabled = false;
+    stopButton.disabled = false;
 }
 
 function stopTimer() {
@@ -29,15 +32,18 @@ function stopTimer() {
     addToLapList();
     resetTimer();
     startButton.disabled = false;
+    stopButton.disabled = true;
 }
 
 function pauseTimer() {
     if(pauseButton.textContent === 'Pause') {
         clearInterval(interval);
         pauseButton.textContent = 'Resume';
+        pauseButton.classList.add("pausePulse");
     } else {
         interval = setInterval(updateTimer, 10);
         pauseButton.textContent = 'Pause';
+        pauseButton.classList.remove("pausePulse");
     }
     // Added logic to resume from Pause
 }
@@ -46,6 +52,10 @@ function resetTimer() {
     clearInterval(interval);
     resetTimerData();
     startButton.disabled = false;
+    pauseButton.classList.remove("pausePulse");
+    pauseButton.textContent = 'Pause';
+    pauseButton.disabled = true;
+    stopButton.disabled = true;
 }
 
 function updateTimer() {
